b92d510e22e746775a5d3916fdd78084c013ea71
tomohiko-urai <84952494+tomohiko-urai@users.noreply.github.com>
2024-11-28 16:17:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ cap = cv2.VideoCapture(12)
 #cap = cv2.VideoCapture(2)
 #cap = cv2.VideoCapture(1)
 #cap = cv2.VideoCapture(0)
-cv2.namedWindow("Camera")
+#cv2.namedWindow("Camera")
 cv2.setMouseCallback("Camera", on_mouse)
 
 while True:
